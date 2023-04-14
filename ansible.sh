e483cf0374f49c4db5931723440ae9816a5138a6
Prashanth2Paramaah <122254628+Prashanth2Paramaah@users.noreply.github.com>
2023-04-14 18:31:21 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -12,10 +12,17 @@ sudo yum -y update
 
 sudo yum -y install ansible
 
+#----------------------------------------------
 #Install Docker
+
 sudo yum install -y yum-utils
+
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 sudo yum install docker-ce docker-ce-cli containerd.io
+
 sudo systemctl start docker
+
 sudo systemctl enable docker
+
 # systemctl restart docker containerd
